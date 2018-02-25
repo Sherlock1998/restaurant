dec0d7e9f00c8bebcd53de216058c0a28c75bf5e
Yoong Jian <iuncleblade_yj@hotmail.com>
2018-02-25 22:34:49 +0800

    created hero section

---
+++ src/components/hero.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Hero extends Component{
+  render() {
+    return (
+      <section className="hero">
+        
+        <div className="hero__heading">
+          <h1 className="hero__heading--primary">La Granda</h1>
+          <h4 className="hero__heading--secondary">experience fine italian delights</h4>
+        </div>
+      </section>
+    )
+  }
+}
