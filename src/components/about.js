74eb5935b8dba2d6737c3f7611470bddddc2ab6f
Yoong Jian <iuncleblade_yj@hotmail.com>
2018-03-11 15:00:14 +0800

    finished front end for mobile and desktop

--- src/components/about.js
+++ src/components/about.js
@@ -6,16 +6,16 @@ export default class About extends Component {
       <section className="overlap-section">
         <div className="overlap">
           <div className="reservation">
-            <div className="col-4-md col-12-xs reservation__text">
+            <div className="reservation__text">
               <h4 className="reservation__text--heading heading--primary">Reservations</h4>
               <p className="body__text">Contact us now to book a table. Reservations must be made on the same day</p>
               <button className="reservation__text--button">book a table</button>
             </div>
-            <div className="col-8-md col-12-xs reservation__pic"></div>
+            <div className="reservation__pic"></div>
             </div>
           <div className="about">
-          <div className="col-4-md  col-12-xs about__pic"></div>
-            <div className="col-8-md about__text">
+          <div className="about__pic"></div>
+            <div className="about__text">
               <h4 className="about__text--heading heading--primary">About</h4>
               <p className="body__text">La Granda is a third generation family owned restaurant that have received numerous fine-dining awards over the years. The restaurant is well known for preserving its authentic flavours throughout the years.</p>
             </div>
